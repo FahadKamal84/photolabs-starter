a4c18258cfa2fc79b9a774e6b3df2d700f2edb0b
Fahad Kamal <fahadkamal@hotmail.com>
2023-10-05 09:53:50 -0400

    build component with provided sample data and bring it in via prop from app

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -3,18 +3,20 @@ import React from "react";
 import "../styles/PhotoListItem.scss";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
-const PhotoListItem = () => {
+
+const PhotoListItem = (props) => {
+
+  const { imageSource, profile, username, city, country } = props
+  return (
+    <article>
+      <img src = {imageSource} />
+      <img src = {profile}/>
+      <span>{username}</span> <br></br>
+      {city}, 
+      {country}
+    </article>
+  )
   /* Insert React */
 };
 
