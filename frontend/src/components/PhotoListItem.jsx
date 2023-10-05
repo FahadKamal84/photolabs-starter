5a59deb2e4d18d20312ff47a919bd46b859b608b
Fahad Kamal <fahadkamal@hotmail.com>
2023-10-05 10:26:33 -0400

    add styling through scss

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -9,12 +9,17 @@ const PhotoListItem = (props) => {
 
   const { imageSource, profile, username, city, country } = props
   return (
-    <article>
-      <img src = {imageSource} />
-      <img src = {profile}/>
-      <span>{username}</span> <br></br>
-      {city}, 
-      {country}
+    <article className = "photo-list__item" >
+      <img className = "photo-list__image" src = {imageSource} />
+      <div className="photo-list__user-details">
+        <img className = "photo-list__user-profile" src = {profile}/>
+        <span className="photo-list__user-info">{username}
+          <p className="photo-list__user-location">
+            {city}, 
+            {country}
+          </p>
+        </span>
+      </div>
     </article>
   )
   /* Insert React */
